f2c6b3922db22493cae8b2772f5cc1c455b850de
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-01-07 00:43:00 +0200

    Delete files

--- Python Advanced/04_Comprehension/Lab/07.py
+++
@@ -1,4 +0,0 @@
-string = input().split("|")
-matrix = [ch for row in string[::-1] for ch in row if not ch.isspace()]
-
-print(" ".join(matrix))
--- Python Advanced/04_Comprehension/Lab/07_var2.py
+++
@@ -1,5 +0,0 @@
-lists = [outer_list.split() for outer_list in input().split("|")]
-
-output_list = [elem for list in reversed(lists) for elem in list]
-
-print(" ".join(output_list))
